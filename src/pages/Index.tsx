e9fc2210185e2438d89f90f0455f923216197523
karhin20 <kelvinarhin25@gmail.com>
2024-12-03 03:42:54 +0000

    text altered

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -70,7 +70,7 @@ const Index = () => {
         {/* Hero Content */}
         <div className="relative container mx-auto h-full flex flex-col justify-center items-center text-center px-4">
           <h2 className="text-sm uppercase tracking-wider mb-4 text-white font-bold animate-fade-in">
-            Welcome to The Apostolic Church - Ghana <br /> One Fold, One Shepherd
+            Welcome to The Apostolic Church - Ghana
           </h2>
           <h1 className="text-4xl md:text-6xl font-bold mb-8 leading-tight animate-fade-in delay-100 text-church-secondary">
             NII BOIMAN<br />CENTRAL ASSEMBLY
