9ea07b5044dd13ceda66593307b4e04826f4e04a
karhin20 <kelvinarhin25@gmail.com>
2024-12-17 00:42:30 +0000

    slow load solved

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App'
 import './index.css'
 import { register } from './serviceWorkerRegistration'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
