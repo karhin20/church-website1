ee03108787820929852e60717a136bb38a7f61f4
karhin20 <kelvinarhin25@gmail.com>
2024-12-18 00:13:36 +0000

    pwa fximg

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      strategies: 'injectManifest',
+      srcDir: 'src',
+      filename: 'serviceWorkerRegistration.ts',
+      injectRegister: false, // We're using our own registration
+      manifest: false, // We'll use our own manifest.json from public folder
+      injectManifest: {
+        injectionPoint: undefined
+      }
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
